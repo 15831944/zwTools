cdfd164225a24e9070f49f506bf1c24dc4e72fad
wuguo088 <wuguo088@163.com>
2016-07-31 20:44:14 +0800

    改关于

--- zwTools/FormAbout.Designer.cs
+++ zwTools/FormAbout.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.btnYes = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnYes
             // 
-            this.btnYes.Location = new System.Drawing.Point(180, 125);
+            this.btnYes.Location = new System.Drawing.Point(197, 126);
             this.btnYes.Name = "btnYes";
             this.btnYes.Size = new System.Drawing.Size(75, 23);
             this.btnYes.TabIndex = 0;
@@ -45,17 +46,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 45);
+            this.label1.Location = new System.Drawing.Point(12, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 36);
+            this.label1.Size = new System.Drawing.Size(89, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "有什么好看的。\r\n\r\nby ZW";
+            this.label1.Text = "有什么好看的。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "by zhangwei6";
             // 
             // FormAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnYes);
             this.MaximizeBox = false;
@@ -71,5 +82,6 @@
 
         private System.Windows.Forms.Button btnYes;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
